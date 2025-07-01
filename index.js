f64e6b65a18f225b500c0397e35d2632e31bd571
Lorenzo Pizzi <lorenzo.pizzi@oclock.school>
2025-07-01 16:21:38 +0200

    header ok

---
+++ index.js
@@ -0,0 +1,23 @@
+import "dotenv/config";
+import express from "express";
+import { Sequelize } from "sequelize";
+
+export const sequelize = new Sequelize(process.env.PG_URL, {
+  dialect: "postgres",
+  logging: false,
+});
+
+try {
+  await sequelize.authenticate();
+  console.log("✅ Connexion à la base de données réussie !");
+} catch (error) {
+  console.error("❌ Impossible de se connecter à la base de données :", error);
+  process.exit(1);
+}
+
+const PORT = process.env.PORT || 3000;
+const app = express();
+
+app.listen(PORT, () => {
+  console.log(`🚀 Server running at http://localhost:${PORT}`);
+});
