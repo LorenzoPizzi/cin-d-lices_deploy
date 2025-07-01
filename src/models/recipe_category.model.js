971f2d9f146e5e1e1b4696ce96d4a9a2d85c6548
Pierre Valadeau <pierrevaladeau@MacBookAir.lan>
2025-07-01 16:04:01 +0200

    edit index.js

--- src/models/recipe_category.model.js
+++ src/models/recipe_category.model.js
@@ -1,5 +1,5 @@
 import { DataTypes } from "sequelize";
-import { sequelize } from "../index.js";
+import { sequelize } from "/index.js";
 
 const RecipeCategory = sequelize.define("RecipeCategory", {
   id_recipe: {
