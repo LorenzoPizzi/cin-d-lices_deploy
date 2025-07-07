67a6f99d7d01f02d21aa0079adbf6efa85582f82
MorenoLuc88 <moreno.luc88@gmail.com>
2025-07-07 11:34:18 +0200

    feat(luc): auth routes

--- src/routes/router.js
+++ src/routes/router.js
@@ -1,9 +1,11 @@
 import { Router } from "express";
 import recipeRoutes from "./recipe.routes.js";
 import categoryRoutes from "./category.routes.js";
+import authRoutes from "./auth.routes.js";
 
 const router = Router();
 
+router.use("/", authRoutes);
 router.use("/", recipeRoutes);
 router.use("/categories", categoryRoutes);
 
