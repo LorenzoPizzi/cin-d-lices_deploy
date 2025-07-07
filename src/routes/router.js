b03c4b6f17498f2cc85b2d26d293ef83474d16f5
PierreValadeau <p.valadeau@outlook.fr>
2025-07-07 09:18:06 +0200

    fixing routes homepage/recipes

--- src/routes/router.js
+++ src/routes/router.js
@@ -6,11 +6,15 @@ import apiRoutes from "./api.routes.js";
 
 const router = Router();
 
-router.use("/", recipeRoutes); 
+router.use("/recipes", recipeRoutes); 
 
 router.use("/categories", categoryRoutes);
 
-router.use("/api", apiRoutes);  
+router.use("/api", apiRoutes); 
+
+router.get("/", (req, res) => {
+    res.redirect("/recipes");
+}); 
 
 
 export default router;
