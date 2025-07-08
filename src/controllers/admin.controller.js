28d731a30d125dd0965687a7583876c2a7877cc7
PierreValadeau <p.valadeau@outlook.fr>
2025-07-08 10:17:37 +0200

    fiwing user nb of recipe and sup redirection

--- src/controllers/admin.controller.js
+++ src/controllers/admin.controller.js
@@ -5,9 +5,16 @@ import Recipe from "../models/recipe.model.js";
 const adminController = {
   showAdminPage: async (req, res) => {
     try {
-      const users = await User.findAll();
+      const users = await User.findAll({
+        attributes: { exclude: ["password", "token"] },
+        include: [{ model: Recipe, as: "recipes", attributes: ["id_recipe"] }],
+      });
+     
+      
+
       const categories = await Category.findAll();
       const recipes = await Recipe.findAll();
+
       res.render("adminpage", { users, categories, recipes });
     } catch (error) {
       console.error("Erreur dans adminController:", error);
@@ -17,3 +24,4 @@ const adminController = {
 };
 
 export default adminController;
+
