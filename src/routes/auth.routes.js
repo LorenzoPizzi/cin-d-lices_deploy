64579343b312a72993d15be66603d7678f99ea42
VirakCode <virak.dok@oclock.school>
2025-07-08 10:42:15 +0200

    guest button fixed

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -3,7 +3,13 @@ import { login, register, logout } from "../controllers/auth.controller.js";
 
 const router = Router();
 
+router.get('/login', (req, res) => {
+    res.render('loginpage');
+});
 router.post("/login", login);
+router.get('/register', (req, res) => {
+    res.render('registerpage');
+});
 router.post("/register", register);
 router.get("/logout", logout);
 
