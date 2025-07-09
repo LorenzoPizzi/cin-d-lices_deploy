a44b2f787ba9e786c71282ad5d75145479ddbfea
PierreValadeau <p.valadeau@outlook.fr>
2025-07-09 12:17:05 +0200

    fixing register and login

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -4,11 +4,13 @@ import { login, register, logout } from "../controllers/auth.controller.js";
 const router = Router();
 
 router.get("/login", (req, res) => {
-    res.render("loginpage");
+    res.locals.style = 'login';
+    res.render("login");
 });
 router.post("/login", login);
 router.get("/register", (req, res) => {
-    res.render("registerpage");
+    res.locals.style = 'register';
+    res.render("register");
 });
 router.post("/register", register);
 router.get("/logout", logout);
