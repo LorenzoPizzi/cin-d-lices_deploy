ce14276bce601dcfc6dbbbf29eda70a372551053
MorenoLuc88 <moreno.luc88@gmail.com>
2025-07-15 14:38:41 +0200

    fix contact page message

--- src/controllers/contact.controller.js
+++ src/controllers/contact.controller.js
@@ -3,7 +3,7 @@ import { sendEmail } from "../utils/sendMail.js";
 
 export async function handleContactForm(req, res) {
     const { name, email, message } = req.body;
-    res.locals.style = 'error';
+    res.locals.style = "error";
 
     if (!name || !email || !message) {
         return res.status(StatusCodes.BAD_REQUEST).render("errorpage", {
@@ -23,18 +23,18 @@ ${message}
 
         await sendEmail("contact@demomailtrap.co", subject, text);
 
-        return res.status(StatusCodes.OK).render("errorpage", {
+        return res.status(StatusCodes.OK).render("error", {
             message:
                 "Merci pour votre message ! Nous reviendrons vers vous rapidement.",
             isSuccess: true,
+            style: "error",
         });
     } catch (error) {
-        return res
-            .status(StatusCodes.INTERNAL_SERVER_ERROR)
-            .render("errorpage", {
-                message:
-                    "Erreur lors de l'envoi du message. Merci de réessayer plus tard.",
-                isSuccess: false,
-            });
+        return res.status(StatusCodes.INTERNAL_SERVER_ERROR).render("error", {
+            message:
+                "Erreur lors de l'envoi du message. Merci de réessayer plus tard.",
+            isSuccess: false,
+            style: "error",
+        });
     }
 }
