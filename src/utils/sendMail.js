448eaf14ff80fd0264f15ecea9d14b121f1e2963
MorenoLuc88 <moreno.luc88@gmail.com>
2025-07-15 16:22:14 +0200

    send mail with google account

--- src/utils/sendMail.js
+++ src/utils/sendMail.js
@@ -2,17 +2,18 @@ import "dotenv/config";
 import nodemailer from "nodemailer";
 
 const transporter = nodemailer.createTransport({
-    host: "sandbox.smtp.mailtrap.io",
-    port: 587,
+    host: process.env.SMTP_HOST,
+    port: Number(process.env.SMTP_PORT),
+    secure: process.env.SMTP_SECURE === "true",
     auth: {
-        user: process.env.MAILTRAP_USER,
-        pass: process.env.MAILTRAP_PASSWORD,
+        user: process.env.SMTP_USER,
+        pass: process.env.SMTP_PASSWORD,
     },
 });
 
 export async function sendEmail(to, subject, text) {
     const mailOptions = {
-        from: '"Ciné Délices" <noreply@demomailtrap.co>',
+        from: `"Ciné Délices" <${process.env.SMTP_USER}>`,
         to,
         subject,
         text,
